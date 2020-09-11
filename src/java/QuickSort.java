454d4829d7dc3ed394b17895fc05b2b87d5b9fc9
Luke <a123@luke.local>
2020-09-11 12:56:40 +0800

    new pull request

--- src/java/QuickSort.java
+++ src/java/QuickSort.java
@@ -1,6 +1,7 @@
 public class QuickSort {
     public static void main(String[] args) {
         System.out.println("This is the test branch");
+        System.out.println("another test for pull request");
         System.out.println("你好，世界！");
     }
 }
